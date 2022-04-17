642d575554ad6e0d166bfe62b27384775fbcfb73
lburgosr1 <luismanuelbr.2007@gmail.com>
2022-04-17 14:15:38 -0400

    Added sidenav, toolbar and icon modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AddHeroComponent } from './pages/add-hero/add-hero.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
 import { HeroesHomeComponent } from './pages/heroes-home/heroes-home.component';
 import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
-
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -19,6 +21,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
